0f7c98428fde577b32fd96a78583c2b8fe091e16
taminhluan <ngoalongtb001@gmail.com>
2025-03-05 12:53:54 +0700

    install missing dependencies if need

--- __init__.py
+++ __init__.py
@@ -1,4 +1,18 @@
-from .main import TLGeoQGISPlugin
+try :
+    import flask
+    import flask_cors
+except ImportError:
+    import sys
+    import os
+    import subprocess
 
+    qgis_executable = sys.executable  # This gives '/Applications/QGIS.app/Contents/MacOS/QGIS'
+    qgis_base = os.path.dirname(qgis_executable)  # Move up one level
+    qgis_python = os.path.join(qgis_base, "bin", "python3")  # Append 'bin/python3'
+
+    subprocess.run([qgis_python, '-m', 'pip', 'install', 'Flask'])
+    subprocess.run([qgis_python, '-m', 'pip', 'install', 'flask-cors'])
+
+from .main import TLGeoQGISPlugin
 def classFactory(iface):
     return TLGeoQGISPlugin(iface)
