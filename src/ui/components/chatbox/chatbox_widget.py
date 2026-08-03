1f64a4090a51bfe0b00ce9c5f89900413b8074c4
taminhluan <ngoalongtb001@gmail.com>
2026-08-03 13:28:01 +0700

    style: double height of chatbox input area to 48px base height for generous spacious layout

--- src/ui/components/chatbox/chatbox_widget.py
+++ src/ui/components/chatbox/chatbox_widget.py
@@ -275,9 +275,9 @@ class ChatInputEdit(QPlainTextEdit):
         self.setPlaceholderText("")
         self.setVerticalScrollBarPolicy(Qt.ScrollBarAsNeeded)
         self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-        self.document().setDocumentMargin(3)
+        self.document().setDocumentMargin(8)
         self.textChanged.connect(self.adjust_height)
-        self.setFixedHeight(24)
+        self.setFixedHeight(48)
         
     def keyPressEvent(self, event):
         # Send message on Enter, but insert new line on Shift+Enter
@@ -292,10 +292,10 @@ class ChatInputEdit(QPlainTextEdit):
     def adjust_height(self):
         doc = self.document()
         doc_height = doc.size().height()
-        clamped_height = max(24, min(int(doc_height), 42))
+        clamped_height = max(48, min(int(doc_height), 80))
         self.setFixedHeight(clamped_height)
         if self.parent_container:
-            self.parent_container.setFixedHeight(clamped_height + 8)
+            self.parent_container.setFixedHeight(clamped_height + 12)
 
 class ChatBox(QWidget):
     """
@@ -414,9 +414,9 @@ class ChatBox(QWidget):
             }
         """)
         self.input_container.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
-        self.input_container.setFixedHeight(32)
+        self.input_container.setFixedHeight(60)
         container_layout = QHBoxLayout(self.input_container)
-        container_layout.setContentsMargins(10, 4, 6, 4)
+        container_layout.setContentsMargins(10, 6, 6, 6)
         container_layout.setSpacing(6)
 
         self.input_field = ChatInputEdit()
